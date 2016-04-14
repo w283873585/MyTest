b484169ec025fbfd454dc1cf9725575ccfab4c81
xnxs <xnxs@xnxs-PC>
2016-04-14 19:03:16 +0800

    1231

--- zTest/src/learn/annotation/Magic.java
+++ zTest/src/learn/annotation/Magic.java
@@ -2,9 +2,12 @@ package learn.annotation;
 
 import java.lang.annotation.Annotation;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
+// 元注解
+@Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE,
 	ElementType.ANNOTATION_TYPE,
 	ElementType.CONSTRUCTOR,
