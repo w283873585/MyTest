157622121bc6884218d645ecd9ba45db0f359863
xnxs <xnxs@xnxs-PC>
2016-04-18 16:49:35 +0800

    a

---
+++ zTest/src/learn/stream/util/StreamUtil.java
@@ -0,0 +1,35 @@
+package learn.stream.util;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class StreamUtil {
+	public static interface Task{
+		public void invoke(byte[] b);
+	}
+	public static void out(InputStream input, int capacity, Task task) {
+		byte[] buff = new byte[capacity];
+		int i = 0;
+		try {
+			while (i != -1) {
+				i = input.read(buff);
+				task.invoke(buff);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void out(InputStream input, Task task) {
+		out(input, 1024, task);
+	}
+	public static void main(String[] args) throws Exception {
+		URL url = new URL("http", "www.vrshow.com", 80, "");
+		InputStream input = url.openStream();
+		StreamUtil.out(input, new Task() {
+			public void invoke(byte[] b) {
+				System.out.println(new String(b));
+			}
+		});
+		input.close();
+	}
+}
