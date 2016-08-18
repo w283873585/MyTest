60b406bc6026c4453a26b77c71d7366408bf536f
xnxs <xnxs@xnxs-PC>
2016-08-18 14:15:20 +0800

    132

---
+++ VR_Tool/src/vr/com/restManager/InterfaceParam.java
@@ -0,0 +1,37 @@
+package vr.com.restManager;
+
+import java.util.List;
+
+import vr.com.processor.ValueProcessor;
+
+public class InterfaceParam {
+	private String name;
+	
+	private String desc;
+	
+	private List<ValueProcessor> processors;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public List<ValueProcessor> getProcessors() {
+		return processors;
+	}
+
+	public void setProcessors(List<ValueProcessor> processors) {
+		this.processors = processors;
+	}
+}
