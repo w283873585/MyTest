b9afe64ccd3e624e42655b8159ff865fd9e2e5e2
xnxs <xnxs@xnxs-PC>
2016-09-23 18:29:06 +0800

    333

--- zTest/src/learn/pattern/client/commandChain/Client.java
+++ zTest/src/learn/pattern/client/commandChain/Client.java
@@ -6,6 +6,17 @@ import java.io.InputStreamReader;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
+		/**
+		 * 
+		 * 
+		 * Invoker根据commandStr产生commandVO对象,
+		 * 
+		 * 再根据commandVO对象与CommandEnum对象, 生成Command(用来产生commandName链的工具对象).
+		 * 
+		 * 然后执行command, 即执行commandName链, 根据链上的单个commandName对象返回正确的结果.
+		 * 
+		 */
+		
 		Invoker invoker = new Invoker();
 		while (true) {
 			System.out.print("#");
