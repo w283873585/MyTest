b324f48cb54c0f5d820e302e0bb418c509d1cb05
xnxs <xnxs@xnxs-PC>
2016-12-23 17:15:32 +0800

    aop demo modify

--- zweb/src/main/java/vr/com/aop/LogService.java
+++ zweb/src/main/java/vr/com/aop/LogService.java
@@ -3,14 +3,16 @@ package vr.com.aop;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
+@Component
 @Aspect
 public class LogService {
 	
 	@Pointcut("execution(* *())")
 	private void log() {}
 	
-	@Before("log()")
+	@Before("execution(* *())")
 	public void beforeLog() {
 		System.out.println("天下之大");
 	} 
