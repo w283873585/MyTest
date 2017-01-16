3673b6200689b5820bdc8006d6fc79d75fdaf4fd
xnxs <xnxs@xnxs-PC>
2017-01-16 19:01:25 +0800

    netty start

---
+++ learn/src/main/java/jun/learn/tools/netty/discard/DiscardServer.java
@@ -0,0 +1,56 @@
+package jun.learn.tools.netty.discard;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class DiscardServer {
+	
+	private int port;
+	
+	public DiscardServer(int port) {
+		this.port = port;
+	}
+	
+	public void run() throws Exception{
+		
+		
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup)
+			.channel(NioServerSocketChannel.class)
+			.childHandler(new ChannelInitializer<SocketChannel>() {
+
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					// TODO Auto-generated method stub
+					ch.pipeline().addLast(new DiscardServerHandler());
+				}
+			})
+			.option(ChannelOption.SO_BACKLOG, 128)
+			.childOption(ChannelOption.SO_KEEPALIVE, true);
+			
+			ChannelFuture f = b.bind(port).sync();
+			
+			// Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+			f.channel().closeFuture().sync();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new DiscardServer(8088).run();
+	}
+}
